10671989dcc8261c2a1c8294fe889d206de3e72b
Yingdong Li <liyingdong@caicloud.io>
2020-07-03 14:29:27 +0800

    Update ssr.js

--- server/ssr.js
+++ server/ssr.js
@@ -6,7 +6,6 @@ import { renderToString } from "react-dom/server";
 import Foo from "../src/foo";
 const path = require("path");
 const fs = require("fs");
-import App from '../src/App'
 
 // global
 // let clientRoutes = []
@@ -51,17 +50,17 @@ export default (app) => {
             res.redirect(302, redirect.pathname + redirect.search);
           } else if (ssrData) {
             // components 的第一个元素就是 layout 组件, 相当于 next.js 中 page
-            const Container = ssrData.components[0]
+            const Container = ssrData.components[0];
             let props = Container.getInitialProps
               ? await Container.getInitialProps()
               : {};
-            ssrData.components[0] = p => {
-              const initialProps = {...p, ...props}
-              return <Container {...initialProps} />
-            }
+            ssrData.components[0] = (p) => {
+              const initialProps = { ...p, ...props };
+              return <Container {...initialProps} />;
+            };
 
             // console.log("ssrData", ssrData);
-          
+
             let Component = createElement(RouterContext, ssrData);
 
             const componentContent = renderToString(Component);
@@ -84,41 +83,6 @@ export default (app) => {
           }
         }
       );
-      return;
-      // console.log(branch);
-      // //得到要渲染的组件
-      console.log(branch);
-      const Component = branch[0].route.component;
-      // var RenderFunction
-      // const componentFunString = `RenderFunction = ${Component}`
-      // console.log(componentFunString)
-      // eval(componentFunString)
-      //将组件渲染为 html 字符串
-      try {
-        let props = Component.getInitialProps
-          ? await Component.getInitialProps()
-          : {};
-
-        console.log({ props });
-        const componentContent = renderToString(<Component {...props} />);
-
-        const mainContent = renderToString(
-          <RenderUI content={componentContent} state={props} />
-        );
-
-        console.log(mainContent);
-        // res.end(mainContent);
-
-        res.end(
-          template.replace(
-            /<div id="root"><\/div>/,
-            `<div id="root">${mainContent}</div>`
-          )
-        );
-      } catch (e) {
-        console.warn(e);
-      }
     }
-    next();
   });
 };
